6e14e624ed08781b2f6e0d4fc14346a3e73ada36
battulga0719 <battulgatulga39@gmail.com>
2024-03-17 18:48:52 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  important: true,
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     fontFamily: {
       sans: ['Roboto'],
+    },
+    container: {
+      center: true
     },
     extend: {},
   },
